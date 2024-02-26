677bc0b118251071d6be2eeffcc053b0dbcc4466
mrjayantbokade <mrjayant81@gmail.com>
2024-02-26 17:53:30 +0530

    tailwind and props

---
+++ 01-tailwind-css-practice/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import Card from './Card'
+
+function App() {
+
+  // let response = {
+  //   username: "jayant",
+  //   std: "3rd year",
+  // }
+
+  // let response = [ "jayant", "4th year"]
+  return (
+    <>
+      {/* <Card obj = "chaiaurcode" hello = {myObj} /> 
+      <Card obj = "chaiaurcode" hello = {myObj} />  */}
+
+      <Card username = "mrjayant81"  />
+      <Card username="mrblues_09" btnText = "visit profile"/>
+       
+    </>
+  )
+}
+
+export default App
